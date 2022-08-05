05e39eb52b3f3af3a95880fc2b85137f0af703e8
kawamataryo <ba068082@gmail.com>
2022-08-06 08:23:00 +0900

    [fix] docker-compose up downを繰り返すことでidの採番がずれてテストが落ちる不具合を修正

--- refactor_code/ruby/docker/service/entrypoint.sh
+++ refactor_code/ruby/docker/service/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 bundle install --path vendor/bundle
-RACK_ENV=development bundle exec rake db:create
-RACK_ENV=development bundle exec rake db:migrate
-RACK_ENV=development bundle exec rake db:seed
+
+if [ -f ./db/development.sqlite3 ]; then
+  RACK_ENV=development bundle exec rake db:reset
+  RACK_ENV=test bundle exec rake db:seed
+else
+  RACK_ENV=development bundle exec rake db:create
+  RACK_ENV=development bundle exec rake db:migrate
+  RACK_ENV=development bundle exec rake db:seed
+  RACK_ENV=test bundle exec rake db:migrate
+  RACK_ENV=test bundle exec rake db:seed
+fi
 
 echo "start application."
 exec "$@"
