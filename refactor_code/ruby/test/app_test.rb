1ed62be4bb501690347f9045c4f60c9a867172da
kawamataryo <ba068082@gmail.com>
2022-08-06 08:23:21 +0900

    [refactor] minitestのバージョンアップに伴うwarningの対応

--- refactor_code/ruby/test/app_test.rb
+++ refactor_code/ruby/test/app_test.rb
@@ -8,24 +8,24 @@ end
 describe "/" do
   it "index" do
     get '/'
-    last_response.must_be_ok
-    last_response.body.must_include 'tea_0'
-    last_response.body.must_include 'tea_1'
-    last_response.body.must_include 'tea_2'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include 'tea_0'
+    _(last_response.body).must_include 'tea_1'
+    _(last_response.body).must_include 'tea_2'
   end
 
   it "search" do
     get '/?q=tea_0'
-    last_response.must_be_ok
-    last_response.body.must_include 'tea_0'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include 'tea_0'
   end
 end
 
 describe "/users/new" do
   it do
     get '/users/new'
-    last_response.must_be_ok
-    last_response.body.must_include 'username'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include 'username'
   end
 end
 
@@ -33,57 +33,57 @@ end
 describe "/login" do
   it do
     get '/login'
-    last_response.must_be_ok
-    last_response.body.must_include 'username'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include 'username'
   end
 end
 
 describe "/settle" do
   it do
     get '/settle'
-    last_response.must_be_ok
+    _(last_response).must_be_ok
   end
 end
 
 describe "signup and login scenario" do
   it do
     post '/users', {login: 'denzow', email: 'denzow@example.com', password: 'denzow_pass'}
-    last_response.status.must_equal 302
+    _(last_response.status).must_equal 302
 
     get 'logout'
-    last_response.status.must_equal 302
+    _(last_response.status).must_equal 302
 
     post '/login', {login: 'denzow', password: 'denzow_pass'}
-    last_response.status.must_equal 302
+    _(last_response.status).must_equal 302
 
     post '/login', {login: 'invalid', password: 'invalid'}
-    last_response.must_be_ok
-    last_response.body.must_include 'Invalid username or password'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include 'Invalid username or password'
   end
 end
 
 describe "buy tea scenario" do
   it do
     post '/users', {login: 'h3poteto', email: 'h3poteto@example.com', password: 'h3poteto_pass'}
-    last_response.status.must_equal 302
+    _(last_response.status).must_equal 302
 
     post '/cart', {teaId: '2', teaAmount: '200'}
-    last_response.status.must_equal 302
+    _(last_response.status).must_equal 302
     get '/'
-    last_response.body.must_include 'Sold'
+    _(last_response.body).must_include 'Sold'
 
     get '/cart'
-    last_response.must_be_ok
-    last_response.body.must_include 'tea_1'
-    last_response.body.must_include '200'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include 'tea_1'
+    _(last_response.body).must_include '200'
 
     get '/checkout'
-    last_response.must_be_ok
-    last_response.body.must_include '4380 yen'
+    _(last_response).must_be_ok
+    _(last_response.body).must_include '4380 yen'
 
     post '/buy', {paymentType: 'poyjp', paymentInfo1: '123456789'}
-    last_response.status.must_equal 302
+    _(last_response.status).must_equal 302
     get '/'
-    last_response.body.must_include 'Teas.'
+    _(last_response.body).must_include 'Teas.'
   end
 end
